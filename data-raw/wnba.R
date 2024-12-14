db8b28508455bb578c727614f7ae97d20f78cfb4
Callie Showalter <cshowalter@smith.edu>
2024-12-13 19:28:02 -0500

    added my function

---
+++ data-raw/wnba.R
@@ -0,0 +1,4 @@
+## code to prepare `wnba` dataset goes here
+wnba <- readr::read_csv("https://raw.githubusercontent.com/fivethirtyeight/WNBA-stats/refs/heads/master/wnba-player-stats.csv")
+wnba <- janitor::clean_names(wnba)
+usethis::use_data(wnba, overwrite = TRUE)
