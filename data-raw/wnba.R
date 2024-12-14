3d33a1c7eacebac284f0e063feb1bea91e3d9eac
Callie Showalter <cshowalter@smith.edu>
2024-12-13 20:20:23 -0500

    fixing up

--- data-raw/wnba.R
+++
@@ -1,4 +0,0 @@
-## code to prepare `wnba` dataset goes here
-wnba <- readr::read_csv("https://raw.githubusercontent.com/fivethirtyeight/WNBA-stats/refs/heads/master/wnba-player-stats.csv")
-wnba <- janitor::clean_names(wnba)
-usethis::use_data(wnba, overwrite = TRUE)
