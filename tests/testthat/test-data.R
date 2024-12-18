227d99177fe770da404bfdc78e7d56a91900f506
Greta Anesko <ganesko3.14@gmail.com>
2024-12-17 21:38:41 -0500

    added test for wnba_stat_grapher, removed non-existent function from readme

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -1,4 +1,10 @@
 test_that("Correct Data", {
   expect_s3_class(get_wnba_player_stats("Caitlin Clark"), "data.frame")
-}
-)
+})
+
+test_that("wnba_stat_grapher returns a ggplot object", {
+  player <- "Sue Bird"
+  stat <- "f_tr"
+  plot <- wnba_stat_grapher(player, stat)
+  expect_s3_class(plot, "ggplot")
+})
